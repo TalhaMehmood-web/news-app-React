314d38db35831df5778d6f1e45750309b65b3e54
TalhaMehmood <talhamehmood991@gmail.com>
2023-05-25 09:05:27 -0700

    Updates

---
+++ precache-manifest.93213c6061783a445d374c0cd66542be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "703ef6351ee38990aa0d",
+    "url": "/news-app-react/static/css/main.cf68bf16.chunk.css"
+  },
+  {
+    "revision": "703ef6351ee38990aa0d",
+    "url": "/news-app-react/static/js/main.f2be273e.chunk.js"
+  },
+  {
+    "revision": "addad4512a9049d9c7f4",
+    "url": "/news-app-react/static/js/runtime~main.f6007e3f.js"
+  },
+  {
+    "revision": "08c1ee2951b099034a53",
+    "url": "/news-app-react/static/js/2.23199fb7.chunk.js"
+  },
+  {
+    "revision": "bf3f3b7061bd69c08914",
+    "url": "/news-app-react/static/js/3.333ed05c.chunk.js"
+  },
+  {
+    "revision": "753d42a87b60d1cf486e4e7892f4241d",
+    "url": "/news-app-react/index.html"
+  }
+];
